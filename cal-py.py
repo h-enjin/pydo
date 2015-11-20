92d4ca405dffe1c531baf83381de5371c72447dc
h-enjin <kraytenjin@gmail.com>
2015-11-20 17:07:03 +0900

    メニュー追加＆TODO追加作成

--- cal-py.py
+++ cal-py.py
@@ -6,10 +6,10 @@
 
 import sqlite3,sys
 
-if __name__ == "__main__":
-	
+def addTodo():
 	db_connecter = sqlite3.connect("data.db")
 
+	print "TODO追加します"
 	mes = sys.stdin.readline().split("\n")
 
 	sql = "insert into todo_list values ('" + mes[0] +  "', 'false')"
@@ -17,5 +17,24 @@ if __name__ == "__main__":
 
 	db_connecter.commit()
 	db_connecter.close()
+	print "追加しました！\n"
+
+def selectMenu():
+	# ループ終了フラグ
+	exit_flg = True
+
+	while exit_flg:
+		print "--MENU--\nadd:TODO追加\nexit:終了"
+		# メニュー選択
+		menu = sys.stdin.readline().split("\n")
+		input_menu = menu[0]
+
+		if input_menu == "add":
+			addTodo()
+		elif input_menu == "exit":
+			exit_flg = False
+
+if __name__ == "__main__":
+	selectMenu()
 
 	print "終了します"
