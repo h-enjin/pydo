9ce87a1613eca55b3c9e9a149f878b282247a46c
h-enjin <kraytenjin@gmail.com>
2015-11-20 17:28:32 +0900

    登録しているTODO全て表示する機能追加

--- cal-py.py
+++ cal-py.py
@@ -6,10 +6,26 @@
 
 import sqlite3,sys
 
+def showAllTodo():
+	db_connecter = sqlite3.connect("data.db")
+	cursor = db_connecter.cursor()
+	cursor.execute("select todo,flg from todo_list")
+	
+	result = cursor.fetchall()
+
+	print "==TODO Lists=="
+	for row in result:
+		print row[0]
+	
+	print ""
+
+	cursor.close()
+	db_connecter.close()
+
 def showTodo():
 	db_connecter = sqlite3.connect("data.db")
 	cursor = db_connecter.cursor()
-	cursor.execute("select * from todo_list where flg='false'")
+	cursor.execute("select todo,flg from todo_list where flg='false'")
 	
 	result = cursor.fetchall()
 
@@ -40,13 +56,15 @@ def selectMenu():
 	exit_flg = True
 
 	while exit_flg:
-		print "--MENU--\nadd:TODO追加\nlist:TODO表示\nexit:終了"
+		print "--MENU--\nadd:TODO追加\nlist:TODO表示\nall:TODO全て表示\nexit:終了"
 		# メニュー選択
 		menu = sys.stdin.readline().split("\n")
 		input_menu = menu[0]
 
 		if input_menu == "add":
 			addTodo()
+		elif input_menu == "all":
+			showAllTodo()
 		elif input_menu == "list":
 			showTodo()
 		elif input_menu == "exit":
